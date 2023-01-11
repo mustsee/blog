02145d5cede5e60a90a626980abab0d8848ebfad
Thomas Sypniewski <thomas@thomass-macbook-pro.home>
2023-01-12 00:45:04 +0100

    Add basic i18n to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import astroI18next from "astro-i18next";
 
 import sitemap from '@astrojs/sitemap';
 
@@ -7,5 +8,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://mustsee.github.io',
   base: '/git-github-course',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), astroI18next()],
 });
