c9dd43da979f962807f1d7ca05030998891ad477
Thomas Sypniewski <thomas@thomass-macbook-pro.home>
2023-01-13 12:54:02 +0100

    Add SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import mdx from '@astrojs/mdx';
 import astroI18next from "astro-i18next";
 
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
+
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mustsee.github.io',
   base: '/git-github-course',
 	integrations: [mdx(), sitemap(), astroI18next()],
+	output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
 });
