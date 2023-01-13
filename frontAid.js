0b4154d5d2191a4d1cf28a241df1b9db72ec8914
Thomas Sypniewski <thomas@thomass-macbook-pro.home>
2023-01-13 12:21:28 +0100

    Add localizePath function to menu

--- frontAid.js
+++ frontAid.js
@@ -1,8 +1,6 @@
 import fs from 'fs';
 import content from './src/frontAid/content.json' assert { type: "json" };
 
-console.log('Inside the frontAid helper')
-
 for (const [key, value] of Object.entries(content)) {
   fs.writeFile(`./public/locales/${key}/translation.json`, JSON.stringify(value), (err) => {
     if (err) throw err
