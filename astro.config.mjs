fd462b9fe7b8213708d4da4bb34e7cffc6b6bfbd
Thomas Sypniewski <thomas@thomass-macbook-pro.home>
2023-01-13 13:26:58 +0100

    Test goatcounter event

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import astroI18next from "astro-i18next";
-
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
-
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mustsee.github.io',
+  site: 'https://mustsee.github.io',
   base: '/git-github-course',
-	integrations: [mdx(), sitemap(), astroI18next()],
-	output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  integrations: [mdx(), sitemap(), astroI18next()],
 });
